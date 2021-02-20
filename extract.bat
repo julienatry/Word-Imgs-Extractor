25e7fed8d8e7d29f952a4b01514ce4d50dc7f4f4
Julien Atry <atryjulien@gmail.com>
2021-02-20 16:43:48 +0100

    Update extract.bat

--- extract.bat
+++ extract.bat
@@ -1,74 +1,78 @@
 @ECHO OFF
 
+::--------------------------------------------------------------------------------------
 SET source=""
 SET outputdir=""
+::--------------------------------------------------------------------------------------
 
-SET tempfolder="%temp%\WordExtractor"
+
+::vars
+SET tempfolder="%temp%\WordImgsExtractor"
 SET wordconv="C:\Program Files\Microsoft Office\Office14\Wordconv.exe"
 
-SET /A c=0
-
+:MakeOutputDir
 MKDIR %outputdir%
 
 :CopyToTemp
 XCOPY /E /I /Y %source% %tempfolder%
 
 :ConvertDocToDocx
+ECHO Beginning doc to docx conversion
 FOR /F "TOKENS=*" %%F IN ('DIR /S /B "%tempfolder%\*.doc"') DO (
-	ECHO %%~fF
+	ECHO Processing %%~nF
 	%wordconv% -oice -nme "%%~fF" "%%~fFx"
 )
-
 
 
 SETLOCAL ENABLEDELAYEDEXPANSION
 
 :ExtractPNG
 FOR /F "TOKENS=*" %%F IN ('DIR /S /B "%tempfolder%\*.docx"') DO ( 
-"C:\Program Files\7-Zip\7z.exe" x "%%~fF" -aou -o"%%~pF\%%~nF" *.png -r > nul
-SET /a c=0
+	"C:\Program Files\7-Zip\7z.exe" x "%%~fF" -aou -o"%%~pF\%%~nF" *.png -r > nul
+	SET c=0
 
-FOR /F "TOKENS=*" %%E IN ('DIR /S /B "%%~dF%%~pF%%~nF\word\media\*.png"') DO (
-SET /a c+=1
-SET imgname=%%~nF_!c!%%~xE
-ECHO !imgname!
-REN "%%~fE" "!imgname!"
-COPY "%%~dE%%~pE\!imgname!" "%outputdir%"
-)
+	FOR /F "TOKENS=*" %%E IN ('DIR /S /B "%%~dF%%~pF%%~nF\word\media\*.png"') DO (
+		SET /a c+=1
+		SET imgname=%%~nF_!c!%%~xE
+		ECHO !imgname!
+		REN "%%~fE" "!imgname!"
+		COPY "%%~dE%%~pE\!imgname!" "%outputdir%"
+	)
 
 )
 
 :ExtractJPG
 FOR /F "TOKENS=*" %%F IN ('DIR /S /B "%tempfolder%\*.docx"') DO ( 
-"C:\Program Files\7-Zip\7z.exe" x "%%~fF" -aou -o"%%~pF\%%~nF" *.jpg -r > nul
-SET /a c=0
+	"C:\Program Files\7-Zip\7z.exe" x "%%~fF" -aou -o"%%~pF\%%~nF" *.jpg -r > nul
+	SET c=0
 
-FOR /F "TOKENS=*" %%E IN ('DIR /S /B "%%~dF%%~pF%%~nF\word\media\*.jpg"') DO (
-SET /a c+=1
-SET imgname=%%~nF_!c!%%~xE
-ECHO !imgname!
-REN "%%~fE" "!imgname!"
-COPY "%%~dE%%~pE\!imgname!" "%outputdir%"
-)
+	FOR /F "TOKENS=*" %%E IN ('DIR /S /B "%%~dF%%~pF%%~nF\word\media\*.jpg"') DO (
+		SET /a c+=1
+		SET imgname=%%~nF_!c!%%~xE
+		ECHO !imgname!
+		REN "%%~fE" "!imgname!"
+		COPY "%%~dE%%~pE\!imgname!" "%outputdir%"
+	)
 
 )
 
 :ExtractJPEG
 FOR /F "TOKENS=*" %%F IN ('DIR /S /B "%tempfolder%\*.docx"') DO ( 
-"C:\Program Files\7-Zip\7z.exe" x "%%~fF" -aou -o"%%~pF\%%~nF" *.jpeg -r > nul
-SET /a c=0
+	"C:\Program Files\7-Zip\7z.exe" x "%%~fF" -aou -o"%%~pF\%%~nF" *.jpeg -r > nul
+	SET c=0
 
-FOR /F "TOKENS=*" %%E IN ('DIR /S /B "%%~dF%%~pF%%~nF\word\media\*.jpeg"') DO (
-SET /a c+=1
-SET imgname=%%~nF_!c!%%~xE
-ECHO !imgname!
-REN "%%~fE" "!imgname!"
-COPY "%%~dE%%~pE\!imgname!" "%outputdir%"
+	FOR /F "TOKENS=*" %%E IN ('DIR /S /B "%%~dF%%~pF%%~nF\word\media\*.jpeg"') DO (
+		SET /a c+=1
+		SET imgname=%%~nF_!c!%%~xE
+		ECHO !imgname!
+		REN "%%~fE" "!imgname!"
+		COPY "%%~dE%%~pE\!imgname!" "%outputdir%"
+	)
+
 )
 
-)
-
-DEL /F /S /Q "%tempfolder%"
+:DeleteTemp
+RMDIR /S /Q "%tempfolder%"
 
 ENDLOCAL
 PAUSE
